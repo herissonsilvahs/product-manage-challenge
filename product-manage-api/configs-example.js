3b71692752b24403cdb9edd6118e5bf97b0dc703
herissonsilvahs <herissonsilvahs@gmail.com>
2018-06-19 22:35:51 -0300

    Fix errors

--- product-manage-api/configs-example.js
+++ product-manage-api/configs-example.js
@@ -1,4 +1,9 @@
+/* 
+ * This file is a example, copy this file to confgis.js 
+ * And add your modifications
+ */
+
 module.exports = {
-    'secret': 'secret-key',
-    'mongo_uri': 'mongodb://user:password@host:port/schema'
+    'secret': 'secret-key', // Key for JWT
+    'mongo_uri': 'mongodb://user:password@host:port/schema' // add URI connection to Mpngo
 };
