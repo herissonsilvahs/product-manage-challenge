0d84983f9431e8c3e1862725978c86c2db297a22
herissonsilvahs <herissonsilvahs@gmail.com>
2018-06-24 19:09:36 -0300

    refactore for promises

--- product-manage-api/middlewares/authenticate.js
+++ product-manage-api/middlewares/authenticate.js
@@ -4,16 +4,35 @@ module.exports = function(request, response, next){
     var token = request.body.token || request.query.token || request.headers['x-access-token'];
 
     if(token){
-        jwt.verify(token, request.app.get('public-key'), 
-            function(error, decoded){
-                if(error)
-                    return response.status(401).json({success:false, message: error});
-
-                request.decoded = decoded;    
-                next();
+        const async_jwt = new Promise(
+            (resolve, reject) => {
+                jwt.verify(
+                    token,
+                    request.app.get('public-key'),
+                    (error, decoded) => {
+                        if(error)
+                            reject(error)
+                        resolve(decoded)
+                    }
+                );
             }
         );
+
+        async_jwt
+        .then((decoded)=>{
+            request.decoded = decoded;    
+            next();
+        })
+        .catch((error)=>{
+            response.status(401).json({
+                success:false,
+                message: error
+            })
+        });
     }else{
-        return response.status(403).send({success: false, message: 'Token not found'});
+        return response.status(403).send({
+            success: false,
+            message: 'Token not found'
+        });
     }
 };
--- product-manage-api/routes/login.js
+++ product-manage-api/routes/login.js
@@ -6,31 +6,55 @@ var jwt = require('jsonwebtoken');
 router.post('/', function(request, response){
     User.findOne({
         email: request.body.email
-    }, function(error, user){
-        if(error)
-            response.status(400).json({message: error});
-
+    })
+    .then((user)=>{
         if(user){
             if(user.password === request.body.password){
-                // get token, token expired in 1 hour
-                jwt.sign(
-                    {user: user.email},
-                    request.app.get('public-key'),
-                    {expiresIn: 60 * 60},
-                    function(error, token){
-                        if(error)
-                            response.status(400).json({message: error});
 
-                        response.status(200).json({success: true, token: token, user_data: user});
+                const async_jwt = new Promise(
+                    (resolve, reject) => {
+                        // get token, token expired in 1 hour
+                        jwt.sign(
+                            {user: user.email},
+                            request.app.get('public-key'),
+                            {expiresIn: 60 * 60},
+                            (error, token) => {
+                                if(error)
+                                    reject(error)
+                                resolve(token)
+                            }
+                        )
                     }
                 );
+
+                async_jwt
+                .then((token) => {
+                    response.status(200).json({
+                        success: true,
+                        token: token,
+                        user_data: user
+                    });
+                })
+                .catch((error) => {
+                    response.status(400).json({message: error});
+                });
+
             }else{
-                response.status(401).json({success: false, message: 'User authentication failed'});
+                response.status(401).json({
+                    success: false,
+                    message: 'User authentication failed'
+                });
             }
         }else{
-            response.status(401).json({success: false, message: 'User authentication failed'});
+            response.status(401).json({
+                success: false,
+                message: 'User authentication failed'
+            });
         }
-    });
+    })
+    .catch((error)=>{
+        response.status(400).json({message: error});
+    })
 });
 
 module.exports = router;
